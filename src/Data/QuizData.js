c7606f329d837940418bd2fd77638f987f0f30cf
prabalbhandary <prabalbhandary97@gmail.com>
2023-07-20 20:39:23 +0545

    first-commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,77 @@
+export const QuizData = [
+    {
+        question: "Who was the first President of the United States?",
+        option: ["Thomas Jefferson", "George Washington", "Abraham Lincoln", "John Adams"],
+        answer: 2
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        option: ["W", "WA", "H2O", "HO"],
+        answer: 3
+    },
+    {
+        question: "Which continent is known as the Dark Continent?",
+        option: ["Africa", "Europe", "Asia", "Australia"],
+        answer: 1
+    },
+    {
+        question: "Who wrote the famous play Romeo and Juliet?",
+        option: ["William Shakespeare", "Oscar Wilde", "George Orwell", "Jane Austen"],
+        answer: 1
+    },
+    {
+        question: "Which movie features a character named Luke Skywalker?",
+        option: ["Star Wars", "The Lord of the Rings", "Harry Potter and the Sorcerer's Stone", "The Matrix"],
+        answer: 1
+    },
+    {
+        question: "In which sport would you perform a slam dunk?",
+        option: ["Soccer", "Basketball", "Tennis", "Baseball"],
+        answer: 2
+    },
+    {
+        question: "What does URL stand for in computing terminology?",
+        option: ["Uniform Resource Locator", "Uniersal Resource Link", "Uniform Reference Locator", "Universal Reference Link"],
+        answer: 1
+    },
+    {
+        question: "Who is often referred to as the Queen of Pop?",
+        option: ["Madonna", "Beyoncé", "Lady Gaga", "Rihana"],
+        answer: 1
+    },
+    {
+        question: "What is the largest mammal on Earth?",
+        option: ["Africian Elephant", "Blue  Whale", "Giraffe", "Polar Bear"],
+        answer: 2
+    },
+    {
+        question: "What is the capital city of Japan?",
+        option: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+        answer: 3
+    },
+    {
+        question: "Which language runs in a web browser?",
+        option: ["Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        option: ["Cenntral Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        option: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        option: ["1996", "1995", "1994", "none of the above"],
+        answer: 2
+    },
+    {
+        question: "Which gas is prepared in the reaction of Dry Ice and Water?",
+        option: ["Nitrogen", "Oxygen", "Carbon, Hydrogen", "none of the above"],
+        answer: 3
+    }
+];
